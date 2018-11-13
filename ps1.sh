3a3adab8041ff9203e4cacca9175075d1f8ec9d3
Al <al.snchz.c@gmail.com>
2018-11-13 08:50:45 -0600

    Update ps1.sh

--- ps1.sh
+++ ps1.sh
@@ -6,7 +6,8 @@ parse_git_branch() {
 }
 
 
-# PS1 is similar to html tags but with color the tag starts like:
+# PS1 is similar to html tags but with color. This is an open tag \[\e[0;32m] and this is a closing tag \[\e[0m\]
+
 # \[\e[0;32m] bla bla \[\e[0m\] 0;32 is color green, space is \[$(tput sgr0)\], \W current dir
 
 export PS1="\[\e[0;32m\]➜  \[\e[0m\]\[$(tput sgr0)\]\[\e[1;34m\]\W\[\e[0m\]\[\e[0;33m\]\$(parse_git_branch): \[\e[0m\]\n   \[$(tput sgr0)\]\[\e[0;36m\]\#:\[\e[0m\]\[$(tput sgr0)\]\[\e[1;37m\]"
