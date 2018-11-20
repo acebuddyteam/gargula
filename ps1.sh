733bc7cd17810889b019294f027d6d63a54fc626
Al <asanchez@nativeka.tk>
2018-11-20 17:14:10 -0600

    Update ps1.sh
    Fixed issue, when no git status the check mark still was being displayed, now it doesn't display unless a git control is initialized

--- ps1.sh
+++ ps1.sh
@@ -1,6 +1,5 @@
 # Git status
 
-
 check_status() {
 
 red="$bold$(tput setaf 1)"
@@ -22,7 +21,8 @@ fi
 }
 
 
-# Git branch in prompt.
+# Git branch in prompt
+
 parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
