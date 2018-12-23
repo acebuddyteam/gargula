335c71aef4781d990438aa6ac68404bc30b15c5a
Al <asanchez@nativeka.tk>
2018-12-23 13:27:08 -0600

    Update ps1.sh
    Added a cleaner structure for the UI

--- ps1.sh
+++ ps1.sh
@@ -7,7 +7,7 @@ green=$(tput setaf 2)
 
 boshka= git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' > /dev/null 2>&1
 
-# assets 𝘟 ✗ Ӽ 𝘟 𝞦 ✔ ✓ 
+# assets 𝘟 ✗ Ӽ 𝘟 𝞦 ✔ ✓ ▲ ➜
 # Checks if something to commit or not
 if git rev-parse --git-dir > /dev/null 2>&1; then
     if ! git status | grep "nothing to commit" > /dev/null 2>&1; then
@@ -30,10 +30,10 @@ check_branch() {
 # PS1 is similar to html tags but with color the tag starts like:
 # \[\e[0;32m] bla bla \[\e[0m\] 0;32 is color green, space is \[$(tput sgr0)\], \W current dir
 
-export PS1="\[\e[0;32m\]➜\[\e[0m\]\[\$(check_status)\] "
+export PS1="\[\e[0;32m\]➜ \[\e[0m\]\[\$(check_status)\] "
        PS1+="\[$(tput sgr0)\]\[\e[1;34m\]\W\[\e[0m\]"
        PS1+="\[\e[0;36m\]\$(check_branch):\[\e[0m\]\n    \[$(tput sgr0)\]"
-       PS1+="\[\e[0;33m\]\#:\[\e[0m\]\[$(tput sgr0)\]\[\e[1;37m\] "
+       PS1+="\[\e[0;33m\] ▲ =\[\e[0m\]\[$(tput sgr0)\]\[\e[1;37m\] "
 
 export CLICOLOR=1
 export LSCOLORS=fxfxBxDxgxegedabagacad
